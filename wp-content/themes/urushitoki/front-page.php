117ba4dad9571aa2329dc776fc16026dd86eced0
mito <raimiti8@gmail.com>
2021-10-11 00:34:02 +0900

    仮アップ（スタイルガイドの確認）

---
+++ wp-content/themes/urushitoki/front-page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php wp_head(); ?>
+</head>
+<body>
+  <h1 class="c-title-header">ページタイトル</h1>
+  <h2 class="c-title">大見出し</h2>
+  <h3 class="c-title">小見出し</h3>
+  <h4 class="c-title">小見出し20px・中央寄せ</h4>
+  <h4 class="c-title">小見出し18px・中央寄せ</h4>
+  <?php wp_footer(); ?>
+</body>
+</html>

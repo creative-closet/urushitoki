d624a5ffa0d869123ef720e898d25b2810770bb6
RIEL <rielmbp@Rie-MBP.local>
2021-10-10 21:18:45 +0900

    中黒のスタイリング #60

--- wp-content/themes/urushitoki/production/php/index.php
+++ wp-content/themes/urushitoki/production/php/index.php
@@ -14,12 +14,6 @@
     <h1>Js Test2</h1>
   </article>
 
-  <div>
-<ul class="c-bullet">
-  <li class="c-bullet__item">日本伝統漆入選</li>
-  <li class="c-bullet__item">金沢市工芸展入選</li>
-</ul>
-</div>
   <?php wp_footer(); ?>
 </body>
 </html>

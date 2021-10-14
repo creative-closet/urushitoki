77230e6528a6e5ad2876b888d0854eef35dd6d4b
atsushi.isai <atsushi_i@branch-plus.co.jp>
2021-10-15 01:47:04 +0900

    insta,youtubeボタンの追加

--- wp-content/themes/urushitoki/functions.php
+++ wp-content/themes/urushitoki/functions.php
@@ -3,6 +3,8 @@
 	//  フォント・CSS・JavaScriptの呼び出し
 	//*****************************************************************
   function mysite_script() {
+    // wp_enqueue_style( 'all', get_theme_file_uri( '/css/all.css'), array(), '1.0.0' );
+    wp_enqueue_style('fontawesome','https://use.fontawesome.com/releases/v5.2.0/css/all.css');
     wp_enqueue_style( 'style', get_theme_file_uri( '/css/style.css'), array(), '1.0.0' );
     wp_enqueue_script( 'scriptjs', get_theme_file_uri( '/js/script.js'), array(), '1.0.0', true );
   }

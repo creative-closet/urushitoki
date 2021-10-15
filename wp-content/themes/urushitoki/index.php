35fe121ff84d60d152e1454827a3e1b0941fd820
Kazu <relakkumalikehotcake@gmail.com>
2021-10-16 07:52:04 +0900

    メニュー機能追加

--- wp-content/themes/urushitoki/index.php
+++ wp-content/themes/urushitoki/index.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <?php wp_head(); ?>
-</head>
-<body>
-  <article class="documentClass">
-    <h1>Css Test</h1>
-  </article>
-  <article id="sampleId">
-    <h1>Js Test2</h1>
-  </article>
 
-  <?php wp_footer(); ?>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<?php wp_head(); ?>
+</head>
+
+<body>
+	<article class="documentClass">
+		<h1>Css Test</h1>
+	</article>
+	<article id="sampleId">
+		<h1>Js Test2</h1>
+	</article>
+
+	<?php get_footer(); ?>
 </body>
+
 </html>
--- wp-content/themes/urushitoki/production/php/functions.php
+++ wp-content/themes/urushitoki/production/php/functions.php
@@ -1,4 +1,14 @@
 <?php
+//#103 メニューをサポートのため追加
+add_action('init',function () {
+	register_nav_menus([
+		'footer-main_nav' => 'フッター:メインナビ',
+		'footer-post_nav' => 'フッター:投稿ナビ',
+		'footer-sns_nav' => 'フッター:SNSナビ',
+		'footer-contact_nav' => 'フッター:お問い合わせナビ',
+	]);
+});
+
   //*****************************************************************
 	//  フォント・CSS・JavaScriptの呼び出し
 	//*****************************************************************
@@ -7,3 +17,4 @@
     wp_enqueue_script( 'scriptjs', get_theme_file_uri( '/js/script.js'), array(), '1.0.0', true );
   }
   add_action('wp_enqueue_scripts','mysite_script');
+

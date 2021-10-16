abc0aa1c62b0a10b24d86623c9076875b49b270e
Kazu <relakkumalikehotcake@gmail.com>
2021-10-17 02:40:26 +0900

    footet:nav-menu機能パーツ化

--- wp-content/themes/urushitoki/functions.php
+++ wp-content/themes/urushitoki/functions.php
@@ -1,6 +1,6 @@
 <?php
 //#103 メニューをサポートのため追加
-add_action('init',function () {
+add_action('init', function () {
 	register_nav_menus([
 		'footer-main_nav' => 'フッター:メインナビ',
 		'footer-post_nav' => 'フッター:投稿ナビ',
@@ -9,12 +9,13 @@ add_action('init',function () {
 	]);
 });
 
-  //*****************************************************************
-	//  フォント・CSS・JavaScriptの呼び出し
-	//*****************************************************************
-  function mysite_script() {
-    wp_enqueue_style( 'style', get_theme_file_uri( '/css/style.css'), array(), '1.0.0' );
-    wp_enqueue_script( 'scriptjs', get_theme_file_uri( '/js/script.js'), array(), '1.0.0', true );
-  }
-  add_action('wp_enqueue_scripts','mysite_script');
+//*****************************************************************
+//  フォント・CSS・JavaScriptの呼び出し
+//*****************************************************************
+function mysite_script()
+{
+	wp_enqueue_style('style', get_theme_file_uri('/css/style.css'), array(), '1.0.0');
+	wp_enqueue_script('scriptjs', get_theme_file_uri('/js/script.js'), array(), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'mysite_script');
 

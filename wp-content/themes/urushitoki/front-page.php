28997538640c69e66fd46b86c3575e28f4e1188f
mito <raimiti8@gmail.com>
2021-10-20 01:37:45 +0900

    スタイルガイド反映

--- wp-content/themes/urushitoki/front-page.php
+++ wp-content/themes/urushitoki/front-page.php
@@ -8,7 +8,7 @@
 </head>
 <body>
   <h1 class="c-title">タイトル</h1><br><br><br>
-  <h1 class="c-title--header">下層ページタイトル</h1><br><br><br>
+  <h1 class="c-title--header" title-english="English">下層ページタイトル</h1><br><br><br>
   <h2 class="c-title-large">大見出し</h2><br><br><br>
   <h3 class="c-title-small">小見出し</h3><br><br><br>
   <h4 class="c-title-small--center">小見出し20px・中央寄せ</h4><br><br><br>

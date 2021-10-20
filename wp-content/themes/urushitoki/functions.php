67d124ddc00e17fcba56ae94e8aed90447fed014
Daiki Fujimoto <dai.fuji.110728@gmail.com>
2021-10-20 23:51:02 +0900

    アイキャッチのURL取得関数追加

--- wp-content/themes/urushitoki/functions.php
+++ wp-content/themes/urushitoki/functions.php
@@ -145,3 +145,17 @@
 
 		register_post_type( 'accessory', $args );
 	}
+
+/*アイキャッチ画像がなければ標準画像を取得する*/
+function get_eyecatch_default(){
+    if (has_post_thumbnail()):
+
+        $id = get_post_thumbnail_id();
+        $img = wp_get_attachment_image_src($id,'large');//画像サイズはlargeかfullどちらか。
+
+    else:
+        $img = array(get_template_directory_uri(). '/assets/img/post-bg.jpg');//ファイルパスは仮
+    endif;
+
+    return $img;
+}

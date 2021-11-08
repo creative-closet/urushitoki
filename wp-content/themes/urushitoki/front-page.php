5cc546cfc12d7a671dbfb823e05fcfd23ccbde82
aoaoaoaoao___________ <z.1a2oyk@gmail.com>
2021-11-08 22:12:51 +0900

    front-page.phpの記述削除

--- wp-content/themes/urushitoki/front-page.php
+++ wp-content/themes/urushitoki/front-page.php
@@ -7,10 +7,6 @@
 	<article class="documentClass">
     	<h1>Css Test</h1>
 	</article>
-	<figure class="p-articleCard">
-		<img class="p-articleCard__image" src="<?php echo get_template_directory_uri(); ?>/production/images/4ぐい吞み.jpg" alt="">
-		<figcaption class="p-articleCard__title">漆への思い</figcaption>
-	</figure>
 	<article id="sampleId">
 			<h1>Js Test2</h1>
 	</article>

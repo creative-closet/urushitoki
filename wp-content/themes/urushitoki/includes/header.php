6fbbde5f219c5460dac5d47ed533442e9b09987a
Daiki Fujimoto <dai.fuji.110728@gmail.com>
2021-11-19 01:06:31 +0900

    ページタイトルをスマートカスタムフィールドの繰り返しに対応

--- wp-content/themes/urushitoki/includes/header.php
+++ wp-content/themes/urushitoki/includes/header.php
@@ -1,26 +1,23 @@
 <?php if (!(is_front_page(  ))):
-	$titleEn = get_post_meta( get_the_ID(), 'title_english', true );
-	$titleDesc = get_post_meta( get_the_ID(), 'title_description', true );
 	$img = get_eyecatch_default();
 ?>
+
 <!-- フロントページ以外のヘッダーのパーツ -->
 <!-- マークアップは仮です。 -->
 <header class="masthead" style="background-image: url('<?php echo $img[0];?>')">
+	<!-- （smart_custum_fieldの繰り返しを使用） -->
+	<?php
+    $title = SCF::get('title');
+	foreach($title as $fields){
+	?>
 	<h1 class="c-title--header" title-english="
-	<?php if($titleEn):?>
-		<?php echo esc_attr($titleEn)?>">
-	<?php else:?>
-		ページタイトルの英訳が入ります">
-	<?php endif;?>
+	<?php echo esc_attr($fields["titleEn"])?>">
 	<?php the_title( );?>
 	</h1>
-	<p>
-	<?php if($titleDesc):?>
-		<?php echo $titleDesc?>
-	<?php else:?>
-		ページの説明文が入ります
-	<?php endif;?>
-	</p>
+	<p><?php echo $fields["titleDesc"]?></p>
+	<?php } ?>
+	<!-- （smart_custum_fieldの繰り返しを使用-ここまで-） -->
+
 	<?php get_template_part('includes/menu'); ?>
 </header>
 <!-- フロントページのヘッダーのパーツ -->

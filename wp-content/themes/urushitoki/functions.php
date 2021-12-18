c567a03e7ae413a322312d92a3e255288fc6f085
aoaoaoaoao___________ <z.1a2oyk@gmail.com>
2021-12-18 23:50:54 +0900

    フォント読み込み消去

--- wp-content/themes/urushitoki/functions.php
+++ wp-content/themes/urushitoki/functions.php
@@ -24,7 +24,6 @@
         wp_enqueue_style( 'style', get_theme_file_uri( './css/style.css'), array(), '1.0.0' );
         wp_enqueue_script( 'scriptjs', get_theme_file_uri( './js/script.js'), array(), '1.0.0', true );
         wp_enqueue_script('jQuery','https://code.jquery.com/jquery-3.4.1.min.js');
-		wp_enqueue_style('Georgia',get_theme_file_uri( './font/Georgia-Bold.woff'), array(), '1.0.0' );//Georgiaの読み込み
     }
     add_action('wp_enqueue_scripts','mysite_script');
 

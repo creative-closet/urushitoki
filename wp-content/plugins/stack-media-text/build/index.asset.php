cb2528c54a3399a01481c1de47087cb629feee54
Kazu <relakkumalikehotcake@gmail.com>
2021-12-24 20:08:48 +0900

    画像テキスト重なりカスタムブロックスタイル適用

--- wp-content/plugins/stack-media-text/build/index.asset.php
+++ wp-content/plugins/stack-media-text/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c8f39e71d84155ae6d7a0b0c28f0d53b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6ebe3c695ed1bf87c0d327998834d177');

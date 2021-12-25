ee7b4476a0cab5d37bf1de340cc0e82ca2a31478
Kazu <relakkumalikehotcake@gmail.com>
2021-12-25 22:42:57 +0900

    footerレスポンシブ対応

--- wp-content/themes/urushitoki/production/php/footer.php
+++ wp-content/themes/urushitoki/production/php/footer.php
@@ -1,114 +1,25 @@
-	<!-- styleはマークアップ完了までの仮置き -->
-	<style>
-		.k-footer{
-			background-image: url("<?php echo get_theme_file_uri('/assets/image/footer-background.jpg'); ?>");
-			background-size: cover;
-		}
-		.c-wrapper--footer{
-			display: flex;
-			flex-direction: column;
-			padding-left:17%;
-			padding-right:17%;
-			height: inherit;
-		}
-		.k-footer ul{
-			list-style: none;
-		}
-		.k-footer a,.k-text--white{
-			text-decoration: none;
-			color: #fff;
-		}
-		.k-footer-nav{
-			display: flex;
-			justify-content: space-between;
-			margin-top: 50px;
-		}
-		.k-contact{
-			margin-top: 50px;
-		}
-		.k-contact__nav{
-			margin-bottom: 30px;
-		}
-		.footer-sns_nav__list{
-			margin-bottom: 30px;
-			width: 420px;
-			background-color: transparent;
-		}
-		.footer-sns_nav__list:hover{
-			background-color: rgba(255, 255, 255, 0.2);
-			transition: all 0.2s;
-		}
-		.k-footer-link{
-			flex-grow: 1;
-		}
-		.k-footer-logo{
-			display: flex;
-			justify-content: space-between;
-			margin: 60px 0;
-		}
-		.k-copyright{
-			margin-top: auto;
-		}
-	</style>
-
-	<!-- タグ・クラス名はマークアップ完了までの仮置き -->
-	<footer class="k-footer">
+	<footer class="p-footer">
 		<div class="c-wrapper--footer">
-			<nav class="k-footer-nav">
-				<div class="k-footer-link">
+			<nav class="p-footer__nav">
+				<div class="p-footer__nav__menu">
 					<?php
 						$menu_name = 'menu_nav';
 						get_template_part('includes/nav-menu', null , $menu_name );
 					?>
 				</div>
 
-				<div class="k-contact">
-					<p class="k-contact__nav"><a href="<?php echo home_url('/contact/');  ?>" class="k-text--white">お問い合わせ・ご依頼</a></p>
-					<p class="k-contact__nav"><a href="<?php echo home_url('/faq/');  ?>" class="k-text--white">よくある質問</a></p>
-					<address class="k-contact__nav k-text--white">076-229-0860</address>
-				</div>
+				<ul class="p-footer__contact">
+					<li class="p-footer__contact__list"><a href="<?php echo home_url('/contact/');  ?>" class="c-text--white">お問い合わせ・ご依頼</a></li>
+					<li class="p-footer__contact__list"><a href="<?php echo home_url('/faq/');  ?>" class="c-text--white">よくある質問</a></li>
+					<li  class="p-footer__contact__list"><address class="c-text--white">076-229-0860</address></li>
+				</ul>
 			</nav>
 
-			<?php /* SNS */
-			$users = get_users(array('orderby'=>'ID','order'=>'ASC'));
-			$have_ul_tag = false;
-			foreach($users as $user) {
-				$user_id = $user->ID;
-				$group = SCF::get_user_meta($user_id,'sns');
-					foreach ((array)$group as $fields ) {
-						if($fields['sns_type'] != "" && $fields['sns_url'] != ""){
-							if(!$have_ul_tag){ ?>
-								<ul class="footer-sns_nav">
-								<?php $have_ul_tag = true;
-							}
-							$my_sns = $fields['sns_type']; ?>
-							<li class="footer-sns_nav__list">
-								<?php if($my_sns == 'Instagram'){ ?>
-									<a href="<?php echo esc_attr($fields['sns_url']) ?>" class="k-menu-insta" target="_blank" rel="noopener noreferrer">
-										<i class="fab fa-instagram"></i><?php echo $fields['sns_name']; ?>
-									</a>
-								<?php }
-								elseif($my_sns == 'Facebook'){ ?>
-									<a href="<?php echo esc_attr($fields['sns_url']) ?>" class="k-menu-facebook" target="_blank" rel="noopener noreferrer">
-										<i class="fab fa-facebook"></i><?php echo $fields['sns_name']; ?>
-									</a>
-								<?php }
-								elseif($my_sns == 'YouTube'){ ?>
-									<a href="<?php echo esc_attr($fields['sns_url']) ?>" class="k-menu-youtube" target="_blank" rel="noopener noreferrer">
-										<i class="fab fa-youtube"></i><?php echo $fields['sns_name']; ?>
-									</a>
-								<?php } ?>
-							</li>
-						<?php }
-					} ?>
-			<?php }
-			if($have_ul_tag){ ?>
-				</ul>
-			<?php } ?>
-			<div class="k-footer-logo">
-				<!-- width heightはマークアップ完了までの仮置き -->
-				<img src="<?php echo get_theme_file_uri('/assets/image/urushitoki-logo.png'); ?>" alt="うるしときロゴ">
-				<p class="k-copyright k-text--white">©︎ 2021 URUSHITOKI ALL RIGHTS RESERVED.</p>
+			<div class="p-footer-logo">
+				<a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer-logo__link">
+					<img src="<?php echo get_theme_file_uri('/assets/image/urushitoki-logo.png'); ?>" alt="うるしときロゴ">
+				</a>
+				<p class="p-footer-logo__copyright c-text--white">©︎ 2021 URUSHITOKI ALL RIGHTS RESERVED.</p>
 			</div>
 		</div>
 

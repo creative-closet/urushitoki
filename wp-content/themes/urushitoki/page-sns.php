6cbe8682c2ff0a88cec6e5835eebd25d31ce2db1
YAT <yat@yat-net.com>
2022-02-21 02:55:01 +0900

    page-sns-scf-get-change

--- wp-content/themes/urushitoki/page-sns.php
+++ wp-content/themes/urushitoki/page-sns.php
@@ -3,41 +3,38 @@
 	<?php get_template_part('/includes/header')?>
 
 		<div class="c-wrapper c-sns__margin">
-		<?php $users = get_users(array('orderby'=>'ID','order'=>'ASC'));
-		foreach($users as $user) {
-			$user_id = $user->ID;?>
 		<?php /* SNS */
-			$group = SCF::get_user_meta($user_id,'sns');
-			foreach ((array)$group as $fields ) {
-				if($fields['sns_type'] != "" && $fields['sns_url'] != ""){ ?>
+			$group = SCF::get('page-sns');
+			foreach ((array)$group as $fields ) :
+				if($fields['sns_page_type'] != "" && $fields['page-sns_url'] != "") : ?>
 					<figure class="p-sns">
-						<?php $my_sns = $fields['sns_type'];
-						if($my_sns == 'Instagram'){ ?>
-							<a href="<?php echo esc_attr($fields['sns_url']) ?>" class="c-sns--insta" target="_blank" rel="noopener noreferrer">
+						<?php $my_sns = $fields['sns_page_type'];
+						if($my_sns == 'Instagram') : ?>
+							<a href="<?php echo esc_attr($fields['page-sns_url']) ?>" class="c-sns--insta" target="_blank" rel="noopener noreferrer">
 								<i class="fab fa-instagram"></i>
 								<span class="c-title__profile">Instagram</span>
 							</a>
-						<?php }
-						elseif($my_sns == 'Facebook'){ ?>
-							<a href="<?php echo esc_attr($fields['sns_url']) ?>"  class="c-sns--facebook" target="_blank" rel="noopener noreferrer">
+						<?php
+						elseif($my_sns == 'Facebook') : ?>
+							<a href="<?php echo esc_attr($fields['page-sns_url']) ?>"  class="c-sns--facebook" target="_blank" rel="noopener noreferrer">
 								<i class="fab fa-facebook-f"></i>
 								<span class="c-sns__title">Facebook</span>
 							</a>
-						<?php }
-						elseif($my_sns == 'YouTube'){ ?>
-							<a href="<?php echo esc_attr($fields['sns_url']) ?>" class="c-sns--youtube" target="_blank" rel="noopener noreferrer">
+						<?php
+						elseif($my_sns == 'YouTube') : ?>
+							<a href="<?php echo esc_attr($fields['page-sns_url']) ?>" class="c-sns--youtube" target="_blank" rel="noopener noreferrer">
 								<i class="fab fa-youtube"></i>
 								<span class="c-title__profile">YouTube</span>
 							</a>
-						<?php } ?>
+						<?php endif; ?>
 						<div class="p-sns__introduction">
-							<p class="c-text--large"><?php echo $fields['sns_name']; ?></p>
-							<p class="c-text"><?php echo $fields['sns_description']; ?></p>
+							<p class="c-text--large"><?php echo $fields['page-sns_name']; ?></p>
+							<p class="c-text"><?php echo $fields['page-sns_description']; ?></p>
 						</div>
 					</figure>
-				<?php }
-			}
-		} ?>
+				<?php endif;
+			endforeach;
+		?>
 
 
 		<!-- 記述内容の表示 -->

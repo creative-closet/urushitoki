73c6bdd322aaddabc2dc50d07e977a034cf5cdfb
RIEL <rielmbp@RIEL-MBP2022.local>
2022-03-30 17:29:27 +0900

    コードリファクタリング

--- wp-content/themes/urushitoki/production/php/header.php
+++ wp-content/themes/urushitoki/production/php/header.php
@@ -2,8 +2,8 @@
 <html lang="ja">
 <head>
 <?php wp_head(); ?>
-</head><meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>URUSHITOKI</title>
-	<div class="c-body-wrapper">
-	<body <?php body_class( $class ); ?>>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body <?php body_class( $class ); ?>>
+<div class="c-body-wrapper">

1172690c4bd75c30c7a9e57af5c68d9b299cb187
Kazu <relakkumalikehotcake@gmail.com>
2022-04-16 20:46:35 +0900

    shop表示数制限変更

--- wp-content/themes/urushitoki/production/php/includes/archive-shop.php
+++ wp-content/themes/urushitoki/production/php/includes/archive-shop.php
@@ -2,7 +2,7 @@
 	<h2 class="c-title u-margin--small">取扱店</h2>
 	<!-- サブクエリインスタンス -->
 	<?php
-	$pager = '4';
+	$pager = '999';
 	$paged =  (get_query_var( 'paged' )) ? absint( get_query_var( 'paged' ) ) : 1;//absint 負の数にならないように絶対値を表示 get_query_var:クエリ変数を取得 pagedは現在のページ送り番号
 	$args_query = array(
 		'post_status'       => 'publish',//公開済の投稿を指定
@@ -44,23 +44,21 @@
 			</dd>
 		</dl>
 
-	<?php
-		endwhile;//(投稿データ出力ループ -end-)
-		if (is_page('about')){
-			$big = 999999999; // need an unlikely integer
-			echo '<div class="p-pager c-flex c-flex--center u-margin-top--large">';
-			echo paginate_links( array(
-				'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),//ページ番号付きのリンクを生成するために使われるベースの URL を指定します。例えば 'http://example.com/all_posts.php%_%' を指定すると、それに含まれる '%_%' は 'format' 引数（下記参照）により置き換えられます。
-				'current'   => max( 1, $paged),//現在のページ番号
-				'total'     => $shop_query->max_num_pages,//全体のページ数 queryの絞り込み条件をmax_num_pagesに反映させる
-				'mid_size'  => 5,//現在のページの両側にいくつの数字を表示するか。ただし現在のページは含みません。
-				'end_size'  => 1,//ページ番号のリストの両端（最初と最後）にいくつの数字を表示するか。
-				'prev_text' => '＜',//前ページへの文字
-				'next_text' => '＞',//次のページへの文字
-			) );
-			echo '</div>';
-		}
+	<?php endwhile;//(投稿データ出力ループ -end-)
+	// 	if (is_page('about')){
+	// 		$big = 999999999; // need an unlikely integer
+	// 		echo '<div class="p-pager c-flex c-flex--center u-margin-top--large">';
+	// 		echo paginate_links( array(
+	// 			'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),//ページ番号付きのリンクを生成するために使われるベースの URL を指定します。例えば 'http://example.com/all_posts.php%_%' を指定すると、それに含まれる '%_%' は 'format' 引数（下記参照）により置き換えられます。
+	// 			'current'   => max( 1, $paged),//現在のページ番号
+	// 			'total'     => $shop_query->max_num_pages,//全体のページ数 queryの絞り込み条件をmax_num_pagesに反映させる
+	// 			'mid_size'  => 5,//現在のページの両側にいくつの数字を表示するか。ただし現在のページは含みません。
+	// 			'end_size'  => 1,//ページ番号のリストの両端（最初と最後）にいくつの数字を表示するか。
+	// 			'prev_text' => '＜',//前ページへの文字
+	// 			'next_text' => '＞',//次のページへの文字
+	// 		) );
+	// 		echo '</div>';
+	// 	}
 	endif;//(投稿データ有無確認 -end-)
-	wp_reset_postdata();
-	?>
+	wp_reset_postdata(); ?>
 	<!-- サブクエリここまで -->
